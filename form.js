faea33cb05d0be30693c272d350e46e92d4a112f
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-04 17:15:04 +0530

    Check valid Email

--- form.js
+++ form.js
@@ -13,3 +13,14 @@ const output = document.querySelector('.salary-output');
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 })
+
+/*Email*/
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function () {
+    let emailRegex = RegExp('^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$');
+    if (emailRegex.test(email.value))
+    emailError.textContent = ""
+    else
+    emailError.textContent = "Email is Incorrect";
+});
